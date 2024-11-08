4cf35fc1ea2598edc5ad5577e50041a71b0e8dc3
adriantech-beep <Adhrianne29@gmail.com>
2024-11-08 08:14:17 +0800

    updated script.js.modified the .then method

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ const quoteOutput = document.querySelector(".quote-output");
 const quoteButton = document.querySelector(".quote-btn");
 const quoteNumber = document.querySelector(".quote-number");
 
+const errorMessage = function () {
+  quoteOutput.textContent = "Error fetching quote";
+};
+
 const getQuote = function () {
-  const request = new XMLHttpRequest();
-  {
-    request.open("GET", "https://api.adviceslip.com/advice");
-    request.send();
-
-    request.addEventListener("load", function () {
-      console.log(this.responseText);
-
-      const { slip } = JSON.parse(this.responseText);
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((data) => {
+      const { slip } = data;
       quoteOutput.innerHTML = slip.advice;
       quoteNumber.textContent = `#${slip.id}`;
+    })
+    .catch((err) => {
+      console.log(err);
+      errorMessage();
     });
-  }
 };
 quoteButton.addEventListener("click", getQuote);
